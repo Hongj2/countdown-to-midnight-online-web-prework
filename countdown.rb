d63792ecdd08ac9416af8aeb84d9ed8388d17925
Jessica Hong <jessicahong.24@gmail.com>
2019-04-06 17:43:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown (sec)
+  sec = 11
+  while sec > 1
+    sec -= 1
+    puts "#{sec} SECOND(S)!"
+  end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (num)
+  num = 5
+  sleep (num)
 end
